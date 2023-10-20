7eb8a244da7209e2f606fab1f541a38021bdfa12
naymurabir <naymurrahman.abir@gmail.com>
2023-10-20 23:28:34 +0600

    Deployed by vercel seccussfully

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ const client = new MongoClient(uri, {
         strict: true,
         deprecationErrors: true,
     }
-});
+})
 
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const brandsCollection = client.db("brandsDB").collection('brands')
 
